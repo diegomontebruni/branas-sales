e2d17937368921a99712f8df2e51f2aec3e82820
Diego Montebruni <montebrunidiego@gmail.com>
2023-04-12 21:07:59 -0300

    Adicionado arredondamento para extension de big decimal

--- src/main/kotlin/com/montebruni/sales/extensions/BigDecimalExtensions.kt
+++ src/main/kotlin/com/montebruni/sales/extensions/BigDecimalExtensions.kt
@@ -2,5 +2,6 @@ package com.montebruni.sales.extensions
 
 import com.montebruni.sales.domain.valueobjects.Amount
 import java.math.BigDecimal
+import java.math.RoundingMode
 
-fun BigDecimal.toAmount() = Amount(this)
+fun BigDecimal.toAmount() = Amount(this.setScale(2, RoundingMode.HALF_EVEN))
