2c8a58b98f43612d25af0c3721982ea35e881ec7
Diego Montebruni <montebrunidiego@gmail.com>
2023-04-24 21:46:44 -0300

    movido freight calculator handle para handle

--- src/main/kotlin/com/montebruni/sales/resource/calculator/freightCalculator/FreightCalculatorHandler.kt
+++
@@ -1,7 +0,0 @@
-package com.montebruni.sales.resource.calculator.freightCalculator
-
-import com.montebruni.sales.resource.calculator.freightCalculator.input.FreightCalculatorInput
-
-interface FreightCalculatorHandler {
-    fun calculate(input: FreightCalculatorInput) : Double
-}
