d57604f04644249cdb6fe66c98bb72b95ee39cae
Diego Montebruni <montebrunidiego@gmail.com>
2023-05-06 17:50:02 -0300

    Adicionado test container para mongodb

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.montebruni"
@@ -14,8 +15,8 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 val mockkVersion = "1.13.4"
 val kotlinLoggingVersion = "3.0.5"
 val springMockkVersion = "3.1.2"
-val swaggerVersion = "3.0.0"
-val swaggerUIVersion = "3.0.0"
+val testContainerVersion = "1.18.0"
+val testMongoDbVersion = "1.18.0"
 
 repositories {
 	mavenCentral()
@@ -27,18 +28,31 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
-	//Swagger
+	// Swagger
 	implementation("org.springdoc:springdoc-openapi-data-rest:1.6.0")
 	implementation("org.springdoc:springdoc-openapi-ui:1.6.0")
 	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.0")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Testcontainer
+	testImplementation("org.testcontainers:junit-jupiter:1.18.0")
+	testImplementation("org.testcontainers:mongodb:$testMongoDbVersion")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude("org.mockito")
 	}
 	testImplementation("io.mockk:mockk:${mockkVersion}")
 	testImplementation("com.ninja-squad:springmockk:${springMockkVersion}")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.testcontainers:testcontainers-bom:$testMongoDbVersion")
+	}
 }
 
 tasks.withType<KotlinCompile> {
