9bfb835bb3c40ae4edd48e5a56e5edfbd5d6f0b8
Diego Montebruni <montebrunidiego@gmail.com>
2023-05-07 12:20:22 -0300

    Ajustado models do postgres

--- src/main/kotlin/com/montebruni/sales/infra/repository/postgresql/model/OrderItemPostgresqlModel.kt
+++ src/main/kotlin/com/montebruni/sales/infra/repository/postgresql/model/OrderItemPostgresqlModel.kt
@@ -2,9 +2,12 @@ package com.montebruni.sales.infra.repository.postgresql.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.util.UUID
 
@@ -16,11 +19,9 @@ data class OrderItemPostgresqlModel(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: UUID = UUID.randomUUID(),
 
-    @Column(name = "order_id")
-    val orderId: UUID,
-
-    @Column(name = "product_id")
-    val productId: UUID,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    val product: ProductPostgresqlModel,
 
     @Column(name = "price")
     val price: Double,
--- src/main/kotlin/com/montebruni/sales/infra/repository/postgresql/model/OrderPostgresqlModel.kt
+++ src/main/kotlin/com/montebruni/sales/infra/repository/postgresql/model/OrderPostgresqlModel.kt
@@ -1,10 +1,17 @@
 package com.montebruni.sales.infra.repository.postgresql.model
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.util.*
 
@@ -16,15 +23,19 @@ data class OrderPostgresqlModel(
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: UUID = UUID.randomUUID(),
 
-    @Column(name = "order_number")
+    @Column(name = "order_number", nullable = false)
     val orderNumber: String,
 
-    @Column(name = "document")
+    @Column(name = "document", nullable = false)
     val document: String,
 
-    @Column(name = "total_amount")
+    @Column(name = "total_amount", nullable = false)
     val totalAmount: Double,
 
-    @Column(name = "coupon_code")
-    val couponCode: String?
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id", referencedColumnName = "id")
+    val coupon: CouponPostgresqlModel?,
+
+    @OneToMany(mappedBy = "order", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val orderItems: MutableList<OrderItemPostgresqlModel> = mutableListOf()
 )
